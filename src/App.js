40e43325eabafba7fbc690c0b5a3cc907ca99724
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-07-04 11:41:25 +0300

    hw_2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { PersonComponent } from './_components/PersonComponent/PersonComponent.j
 function App() {
   return (
     <>
-      <PersonComponent person={{name: "Artem"}}/>
+      <PersonComponent person={{name: "Artem1"}}/>
+      <PersonComponent person={{name: "Artem2"}}/>
+      <PersonComponent person={{name: "Artem3"}}/>
+      <PersonComponent person={{name: "Artem4"}}/>
+      <PersonComponent person={{name: "Artem5"}}/>
     </>
   );
 }
