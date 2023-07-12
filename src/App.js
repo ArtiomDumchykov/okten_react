31360ae7f0c7c8af58b45a9d139fea541f5848a0
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-07-12 23:08:28 +0300

    hw_3_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Users, Launches, Posts} from './components';
-import { UserPosts } from './components/Users';
+// import { Users, Launches, Posts} from './components';
+// import { UserPosts } from './components/Users';
 
 
 function App() {
-  // const [userId, setUserId] = useState(null) 
+
 
   return (
     <>
       <div className='wrapper'>
-        <Posts />
-        {/* <Launches/> */}
-    
-        {/* <Users setUserId={setUserId}/>
-        {!!userId && <UserPosts userId={userId}/>} */}
+
       </div>
     </>
   );
 }
+// function App() {
+//   const [userId, setUserId] = useState(null) 
+
+//   return (
+//     <>
+//       <div className='wrapper'>
+//         {/* <Posts /> */}
+//         {/* <Launches/> */}
+    
+//         <Users setUserId={setUserId}/>
+//         {!!userId && <UserPosts userId={userId}/>}
+//       </div>
+//     </>
+//   );
+// }
 
 export default App;
