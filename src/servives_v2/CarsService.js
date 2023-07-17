aee4b78857d943a58e19e3c286010cf73120a2c3
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-07-17 22:57:24 +0300

    hw_4_3

---
+++ src/servives_v2/CarsService.js
@@ -0,0 +1,63 @@
+import { RequestService } from "./request.service"
+
+export class CarsService extends RequestService {
+
+    static endpoints = {
+        cars: "/cars",
+        carsId(id) {return `/cars/${id}`}
+    }
+     constructor() {
+        super()
+        if (typeof CarsService.instance === "object") {
+            return CarsService.instance
+        }
+        CarsService.instance = this
+    }
+
+    getAll() {
+        const config = {
+            url: CarsService.endpoints.cars,
+            method: "get",
+            headers: {'content-type': 'application/json'},
+        }
+        return RequestService.axiosRequest(config)
+    }
+
+    getCarsId(id) {
+        const config = {
+            url: CarsService.endpoints.carsId(id),
+            method: "get",
+            headers: {'content-type': 'application/json'},
+        }
+        return RequestService.axiosRequest(config)
+    }
+
+    createCar(data) {
+        const config = {
+            url: CarsService.endpoints.cars,
+            method: "post",
+            headers: {'content-type': 'application/json'},
+            data
+        }
+        return RequestService.axiosRequest(config)
+    }
+
+    deleteCar(id) {
+        const config = {
+            url: CarsService.endpoints.carsId(id),
+            method: "delete",
+            headers: {'content-type': 'application/json'},
+        }
+        return RequestService.axiosRequest(config)
+    }
+    
+    updateCar(id, data) {
+        const config = {
+            url: CarsService.endpoints.carsId(id),
+            method: "put",
+            headers: {'content-type': 'application/json'},
+            data
+        }
+        return RequestService.axiosRequest(config)
+    }
+}
