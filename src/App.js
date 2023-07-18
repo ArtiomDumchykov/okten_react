895f164964c58e8137ca88443e57b9acc0b79d14
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-07-18 17:14:33 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import './App.css';
-import { RegisterPage } from './pages/RegisterPage/RegisterPage';
+import { CommentsPage } from './pages/CommentsPage/CommentsPage';
 
 
-// const usersService = new UsersService()
-// async function f () {
-  // const response = await carsService.getAll()
-  // console.log(response);
-  // const response = await carsService.getCarsId(5471)
-  // console.log(response);
-
-  // const response = await carsService.createCar({brand: "Rainmettal", price: 1488, year: 2000})
-  // console.log(response);
-
-  // const response = await carsService.updateCar(5485, {brand: "Rainmettal", price: 7777, year: 2000})
-  // console.log(response);
-  // const response = await carsService.deleteCar(5485)
-  // console.log(response);
-// }
 
 
 function App() {
+  console.log("render");
   return (
-    <>
-    <RegisterPage />
-    </>
+    <div>
+      <CommentsPage/>
+    </div>
   );
 }
 
---
+++ src/pages/CommentsPage/CommentsPage.jsx
@@ -0,0 +1,68 @@
+import React, { useContext, useEffect, useRef } from 'react';
+
+import './CommentsPage.scss';
+
+import { CommentsForm } from '../../components';
+
+import { CommentsService } from '../../servives_v2'
+
+
+const commentsService = new CommentsService(); 
+
+export function CommentsPage() {
+
+
+
+    const postIdRef = useRef('');
+    const emailRef = useRef('');
+    const titleRef = useRef('');
+    const textRef = useRef('');
+
+
+    const valuesForm = () => {
+        return {
+            postId: +getCurrentValue(postIdRef),
+            email: getCurrentValue(emailRef),
+            name: getCurrentValue(titleRef),
+            body: getCurrentValue(textRef),
+        }
+      }
+
+    const handleResetFormRef = (...refs) => {
+        refs?.map(item => item.current.value = "")
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            e.preventDefault();
+            
+            const data = valuesForm()
+            const response = await commentsService.createComments(data)
+      
+            if (response.status === 201) {
+              handleResetFormRef(postIdRef, emailRef, titleRef, textRef)
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    function getCurrentValue(ref) {
+        return ref.current.value
+    }
+
+    return (
+        <div className='wrapper'>
+            <CommentsForm
+                refs={{
+                    postIdRef,
+                    emailRef,
+                    titleRef,
+                    textRef,
+                }}
+                handleSubmit={handleSubmit}
+            />
+        </div>
+    )
+}
