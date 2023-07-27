2f1f4a5727c744d8f51174706ccc31c281b1529f
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-07-27 20:19:40 +0300

    fix

--- src/components/PostersPreview/PostersPreview.jsx
+++ src/components/PostersPreview/PostersPreview.jsx
@@ -2,9 +2,48 @@ import React from 'react'
 
 import styles from './PostersPreview.module.scss'
 import { PosterPreview } from './PosterPreview/PosterPreview';
+
+const utils_casts = {
+    limit: 5,
+    all: null,
+    chunks: {},
+    total: null
+}
+
+const createChunks = (array) => {
+    if (!array || typeof array !== "object") return;
+
+    const totalChunks =  Math.ceil(array.length / utils_casts.limit);
+    
+    for (let i = 1; i <= totalChunks; i++) {
+        const startIndex = (i - 1) * utils_casts.limit;
+        const endIndex = i * utils_casts.limit;
+        utils_casts.chunks[`page_${i}`] = [...array].slice(startIndex, endIndex);
+    }
+
+    const lastChunk = utils_casts.chunks[`page_${totalChunks}`]
+    const preLastChunk = utils_casts.chunks[`page_${totalChunks - 1}`]
+    if (lastChunk.length < 5) {
+        console.log(lastChunk);
+
+        
+    }
+
+}
+
 export function PostersPreview({ casts }) {
 
-    console.log(casts);
+    createChunks(casts)
+
+
+
+    const handlePrev = () => {
+
+    }
+
+    const handleNext = () => {
+
+    }
 
     return (
         <>
