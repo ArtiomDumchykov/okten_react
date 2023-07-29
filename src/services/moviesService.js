2e66890039aa11621714dc478f0922eda89e33f1
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-07-29 12:52:44 +0300

    fix

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -5,6 +5,7 @@ const movieService = {
   getAll: (params) => instanceAxios.get(urls.movie.base, { params: { ...params } }),
   getById: (id) => instanceAxios.get(urls.movie.byId(id)),
   getCredits: (id) => instanceAxios.get(urls.credits.base(id)),
+  getMovieTrailer: (id) => instanceAxios.get(urls.videos.base(id)),
   getVideos: (id) => instanceAxios.get(urls.videos.base(id)),
   search: (query) => instanceAxios.get(urls.search.base, { params: { query } }),
   trendingDay: () => instanceAxios.get(urls.trending.day.base),
